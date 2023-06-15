e32ef0e76ea29c040e4153ecf75deb73bbb55363
Johnny Ruiz <johnny@ruizops.com>
2023-06-14 22:38:36 -0600

    display stories

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,48 @@
-export default function IndexPage() {
+import { get } from "http"
+
+const getStories = async () => {
+    const res = await fetch(
+        "https://hacker-news.firebaseio.com/v0/topstories.json"
+    )
+    const data = await res.json()
+    return data
+}
+
+const getStory = async (id: number) => {
+    const res = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+    )
+    const data = await res.json()
+    return data
+}
+
+const IndexPage = async () => {
+    const stories = await getStories()
+    stories.length = 20 // limit stories to 20
+
     return (
         <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
             <div className="flex max-w-[980px] flex-col items-start gap-2">
                 <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
                     Hacker News Clone with Next.js 13
                 </h1>
-                <p className="max-w-[700px] text-lg text-muted-foreground">
-                    This is a Hacker News clone built with Next.js 13, Tailwind
-                    CSS, and TypeScript. It uses the Hacker News API to fetch
-                    the latest stories, and it uses SWR to cache the results.
-                </p>
+                {stories.map((story: any) => (
+                    <div key={story}>
+                        {getStory(story).then((data) => (
+                            <div className="flex flex-col gap-2">
+                                <a
+                                    href={data.url}
+                                    className="text-xl font-bold leading-tight tracking-tighter md:text-2xl"
+                                >
+                                    {data.title}
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                ))}
             </div>
         </section>
     )
 }
+
+export default IndexPage
