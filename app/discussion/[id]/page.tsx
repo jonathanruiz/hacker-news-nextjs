e1b4d5920594e870aee8117259e12ae9d481d4c7
Johnny Ruiz <johnny@ruizops.com>
2023-06-18 01:02:47 -0600

    display title fo the dicussion

--- app/discussion/[id]/page.tsx
+++ app/discussion/[id]/page.tsx
@@ -1,9 +1,14 @@
-const DiscussionPage = ({ params }: any) => {
+import { getStory } from "@/utils/hackernews"
+
+const DiscussionPage = async ({ params }: any) => {
+    const story = await getStory(params.id).then((data) => data)
+
     return (
-        <div>
-            <h1>Discussion Page</h1>
-            <p>{params.id}</p>
-        </div>
+        <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+            <div className="flex max-w-[980px] flex-col items-start gap-2">
+                <p>{story.title}</p>
+            </div>
+        </section>
     )
 }
 
