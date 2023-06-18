1794a96ea6927c3b946d9d87ef31fcfa4d55c67d
Johnny Ruiz <johnny@ruizops.com>
2023-06-18 00:26:51 -0600

    added layout and discussion dynamic page

---
+++ app/discussion/[id]/page.tsx
@@ -0,0 +1,10 @@
+const DiscussionPage = ({ params }: any) => {
+    return (
+        <div>
+            <h1>Discussion Page</h1>
+            <p>{params.id}</p>
+        </div>
+    )
+}
+
+export default DiscussionPage
