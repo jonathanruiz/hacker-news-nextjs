77159d538e57231e704699ece9b9e6c82959896a
Johnny Ruiz <johnny@ruizops.com>
2023-06-19 17:40:41 -0600

    show story information

--- app/discussion/[id]/page.tsx
+++ app/discussion/[id]/page.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link"
 import { getStory } from "@/utils/hackerNews"
+
+import { Badge } from "@/components/ui/badge"
 
 const DiscussionPage = async ({ params }: any) => {
     const story = await getStory(params.id).then((data) => data)
@@ -6,7 +9,25 @@ const DiscussionPage = async ({ params }: any) => {
     return (
         <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
             <div className="flex max-w-[980px] flex-col items-start gap-2">
-                <p>{story.title}</p>
+                <div className="flex items-center">
+                    <a href={story.url}>
+                        <p className="text-lg font-bold leading-tight tracking-tighter md:text-xl">
+                            {story.title}
+                        </p>
+                    </a>
+                    <Link href="#" className="ml-2">
+                        <span>
+                            by <Badge>{story.by}</Badge>
+                        </span>
+                    </Link>
+                </div>
+                <div>
+                    <Link href={`/discussion/${story.id}`}>
+                        <span className="text-slate-900 dark:text-slate-400">
+                            {story.descendants} comments
+                        </span>
+                    </Link>
+                </div>
             </div>
         </section>
     )
