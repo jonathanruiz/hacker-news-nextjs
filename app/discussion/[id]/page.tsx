846990e0ff006255cd2c552d36225511247fc9c9
Johnny Ruiz <johnny@ruizops.com>
2023-06-19 20:35:22 -0600

    updated to items instead of story

--- app/discussion/[id]/page.tsx
+++ app/discussion/[id]/page.tsx
@@ -4,56 +4,56 @@ import { getItem } from "@/utils/hackerNews"
 import { Badge } from "@/components/ui/badge"
 
 const DiscussionPage = async ({ params }: any) => {
-    const story = await getItem(params.id).then((data) => data)
+    const item = await getItem(params.id).then((data) => data)
 
     return (
         <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
             <div className="flex max-w-[980px] flex-col items-start gap-2">
                 <div className="flex items-center">
-                    <a href={story.url}>
+                    <a href={item.url}>
                         <h2 className="text-lg font-bold leading-tight tracking-tighter md:text-xl">
-                            {story.title}
+                            {item.title}
                         </h2>
                     </a>
                     <div className="ml-2">
                         <span>by </span>
                         <Link href="#">
-                            <Badge>{story.by}</Badge>
+                            <Badge>{item.by}</Badge>
                         </Link>
                     </div>
                 </div>
                 <div>
-                    <Link href={`/discussion/${story.id}`}>
+                    <Link href={`/discussion/${item.id}`}>
                         <span className="text-slate-900 dark:text-slate-400">
-                            {story.descendants} comments
+                            {item.descendants} comments
                         </span>
                     </Link>
                 </div>
-            </div>
-            <div>
-                {story.kids.map((kid: any) => (
-                    <div key={kid}>
-                        {getItem(kid).then((data) => (
-                            <div>
-                                <div className="flex items-center">
+                <div>
+                    {item.kids.map((kid: any) => (
+                        <div key={kid}>
+                            {getItem(kid).then((data) => (
+                                <div>
+                                    <div className="flex items-center">
+                                        <div>
+                                            <Link href="#">
+                                                <Badge>{data.by}</Badge>
+                                            </Link>
+                                        </div>
+                                    </div>
                                     <div>
-                                        <Link href="#">
-                                            <Badge>{data.by}</Badge>
-                                        </Link>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: data.text,
+                                            }}
+                                            className="text-slate-900 dark:text-slate-400"
+                                        />
                                     </div>
                                 </div>
-                                <div>
-                                    <span
-                                        dangerouslySetInnerHTML={{
-                                            __html: data.text,
-                                        }}
-                                        className="text-slate-900 dark:text-slate-400"
-                                    />
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                ))}
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     )
