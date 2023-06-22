78d172c048fddfbf729627f64f9d6b2b8934abdb
Johnny Ruiz <johnny@ruizops.com>
2023-06-21 23:02:14 -0600

    added styling for CardFooter

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -3,25 +3,28 @@ import { getUser } from "@/utils/hackerNews"
 import { Badge } from "@/components/ui/badge"
 import {
     Card,
-    CardContent,
     CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { Icons } from "@/components/icons"
 
 const UserPage = async ({ params }: any) => {
     const user = await getUser(params.id).then((data) => data)
 
     return (
         <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-            <Card>
+            <Card className="w-1/2">
                 <CardHeader>
                     <CardTitle className="text-2xl">{user.id}</CardTitle>
                     <CardDescription>{user.about}</CardDescription>
                 </CardHeader>
-                <CardFooter>
-                    <Badge>{user.karma}</Badge>
+                <CardFooter className="flex space-x-4 text-sm text-muted-foreground">
+                    <div className="flex">
+                        <Icons.chevronUp></Icons.chevronUp>
+                        <Badge>{user.karma}</Badge>
+                    </div>
                     <span>Created {user.created}</span>
                 </CardFooter>
             </Card>
