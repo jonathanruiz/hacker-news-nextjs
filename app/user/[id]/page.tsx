0df33100bb7956ab12f53014a7908d214fd9d60b
Johnny Ruiz <johnny@ruizops.com>
2023-06-21 22:01:14 -0600

    created UserPage

---
+++ app/user/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { getUser } from "@/utils/hackerNews"
+
+import { Badge } from "@/components/ui/badge"
+
+const UserPage = async ({ params }: any) => {
+    const item = await getUser(params.id).then((data) => data)
+
+    return (
+        <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+            <div className="flex max-w-[980px] flex-col items-start gap-2">
+                <div className="flex items-center">
+                    <h2 className="text-2xl font-bold leading-tight tracking-tighter">
+                        {item.id}
+                    </h2>
+                </div>
+                <div>
+                    <p>Created: {item.created}</p>
+                    <p>
+                        Karma: <Badge>{item.karma}</Badge>
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default UserPage
