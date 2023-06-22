817dd4592a2bd6877d6fd84c8a77b55f9835114b
Johnny Ruiz <johnny@ruizops.com>
2023-06-21 22:21:06 -0600

    converted into card

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,29 +1,30 @@
 import { getUser } from "@/utils/hackerNews"
 
 import { Badge } from "@/components/ui/badge"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 const UserPage = async ({ params }: any) => {
-    const item = await getUser(params.id).then((data) => data)
+    const user = await getUser(params.id).then((data) => data)
 
     return (
         <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-            <div className="flex max-w-[980px] flex-col items-start gap-2">
-                <div className="flex items-center">
-                    <h2 className="text-2xl font-bold leading-tight tracking-tighter">
-                        {item.id}
-                    </h2>
-                </div>
-                <div>
-                    <span>Created: {item.created}</span>
-                </div>
-                <div>
-                    <span>Karma: </span>
-                    <Badge>{item.karma}</Badge>
-                </div>
-                <div>
-                    <span>{item.about}</span>
-                </div>
-            </div>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">{user.id}</CardTitle>
+                    <CardDescription>{user.about}</CardDescription>
+                </CardHeader>
+                <CardFooter>
+                    <Badge>{user.karma}</Badge>
+                    <span>Created {user.created}</span>
+                </CardFooter>
+            </Card>
         </section>
     )
 }
