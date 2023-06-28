d9404dc84c8385b7d700d203d5b4de2ed52e14d2
Johnny Ruiz <johnny@ruizops.com>
2023-06-28 17:54:50 -0600

    changed ui in discussion page and unused class names

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const IndexPage = async () => {
 
     return (
         <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-            <div className="flex max-w-[980px] flex-col items-start gap-2">
+            <div className="flex max-w-[980px] flex-col gap-2">
                 <h1 className="font-extrabold leading-tight tracking-tighter sm:text-xl md:text-2xl lg:text-3xl">
                     Hacker News Clone with Next.js 13
                 </h1>
