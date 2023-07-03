eef0ad67114ff28ffeef688ba9832e0f975e046c
Johnny Ruiz <johnny@ruizops.com>
2023-07-02 22:53:21 -0600

    create loading page and skeleton page

---
+++ app/loading.tsx
@@ -0,0 +1,20 @@
+import { ItemSkeleton } from "@/components/item-skeleton"
+
+const Loading = () => {
+    return (
+        <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+            <div className="flex max-w-[980px] flex-col gap-2">
+                <h1 className="font-extrabold leading-tight tracking-tighter sm:text-xl md:text-2xl lg:text-3xl">
+                    Hacker News Clone with Next.js 13
+                </h1>
+                <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+                    {Array.from(Array(20).keys()).map((_, index) => (
+                        <ItemSkeleton key={index} />
+                    ))}
+                </section>
+            </div>
+        </section>
+    )
+}
+
+export default Loading
