3228a71661c83274ae65f8eaed3951131d24f0a8
Johnny Ruiz <johnny@ruizops.com>
2023-07-02 22:16:06 -0600

    Convert story to card (#23)
    * created story-item component
    
    * updated StoryItem to use cards
    
    * fixed typo
    
    * aligned score
    
    * display hostname above title with ranking of story
    
    * added icon for comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import Link from "next/link"
-
-import { getItem, getStories } from "@/lib/hackerNews"
-import { displayRelativeTime, getUrlHostname } from "@/lib/utils"
-import { Badge } from "@/components/ui/badge"
-import { Separator } from "@/components/ui/separator"
-import { Icons } from "@/components/icons"
+import { getStories } from "@/lib/hackerNews"
+import { StoryItem } from "@/components/story-item"
 
 const IndexPage = async () => {
     const stories = await getStories()
@@ -17,67 +12,7 @@ const IndexPage = async () => {
                     Hacker News Clone with Next.js 13
                 </h1>
                 {stories.map((story: any) => (
-                    <div key={story} className="w-full">
-                        {getItem(story).then((data) => (
-                            <div>
-                                <div className="flex items-center">
-                                    <span className="mr-2 text-slate-400">
-                                        {stories.indexOf(story) + 1}.
-                                    </span>
-                                    <span className="mr-2 flex">
-                                        <Icons.chevronUp />
-                                        <Badge>{data.score}</Badge>
-                                    </span>
-                                    <div>
-                                        {
-                                            // Check if there is a url. If so, make it a link. If not,
-                                            // just display the title with a link to the discussion.
-                                            data.url ? (
-                                                <a href={data.url}>
-                                                    <h2 className="m-0 inline-block text-lg font-bold sm:text-lg md:text-xl">
-                                                        {data.title}
-                                                    </h2>
-                                                    <span className="ml-2 inline-block text-slate-400">
-                                                        (
-                                                        {getUrlHostname(
-                                                            data.url
-                                                        )}
-                                                        )
-                                                    </span>
-                                                </a>
-                                            ) : (
-                                                <Link
-                                                    href={`/discussion/${data.id}`}
-                                                >
-                                                    <h2 className="m-0 inline-block text-lg font-bold sm:text-lg md:text-xl">
-                                                        {data.title}
-                                                    </h2>
-                                                </Link>
-                                            )
-                                        }
-                                    </div>
-                                </div>
-                                <div>
-                                    <Link href={`/discussion/${data.id}`}>
-                                        <span className="text-slate-900 dark:text-slate-400">
-                                            {data.descendants} comments
-                                        </span>
-                                    </Link>
-                                    <span className="ml-5">
-                                        {displayRelativeTime(data.time)}
-                                    </span>
-                                    <span className="ml-2">
-                                        by{" "}
-                                        <Link href={`/user/${data.by}`}>
-                                            <Badge>{data.by}</Badge>
-                                        </Link>
-                                    </span>
-                                </div>
-
-                                <Separator className="my-4 h-1" />
-                            </div>
-                        ))}
-                    </div>
+                    <StoryItem key={story.id} story={story} stories={stories} />
                 ))}
             </div>
         </section>
---
+++ components/story-item.tsx
@@ -0,0 +1,74 @@
+import Link from "next/link"
+
+import { getItem } from "@/lib/hackerNews"
+import { displayRelativeTime, getUrlHostname } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Icons } from "@/components/icons"
+
+import { Separator } from "./ui/separator"
+
+export const StoryItem = ({ story, stories }: any) => {
+    return (
+        <div key={story} className="w-full">
+            {getItem(story).then((data) => (
+                <Card className="flex">
+                    <CardHeader>
+                        <CardTitle className="grid justify-items-center">
+                            <Icons.chevronUp />
+                            <span>{data.score}</span>
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div>
+                            {data.url ? (
+                                <a href={data.url}>
+                                    {/* Display ranking number of story */}
+                                    <span className="text-slate-900 dark:text-slate-400">
+                                        {stories.indexOf(story) + 1}.
+                                    </span>
+                                    <span className="ml-5 inline-block text-slate-400">
+                                        {getUrlHostname(data.url)}
+                                    </span>
+                                </a>
+                            ) : null}
+                        </div>
+                        <div className="text-2xl">
+                            <a href={data.url}>
+                                <h2 className="m-0 inline-block text-lg font-bold sm:text-lg md:text-xl">
+                                    {data.title}
+                                </h2>
+                            </a>
+                        </div>
+                        <div className="flex">
+                            <span>
+                                by{" "}
+                                <Link href={`/user/${data.by}`}>
+                                    <Badge>{data.by}</Badge>
+                                </Link>
+                            </span>
+                            <span className="ml-5">
+                                {displayRelativeTime(data.time)}
+                            </span>
+                            <Link
+                                className="ml-5 flex items-center"
+                                href={`/discussion/${data.id}`}
+                            >
+                                <Icons.messageSquare />
+                                <span className="ml-2 text-slate-900 dark:text-slate-400 ">
+                                    {data.descendants}
+                                </span>
+                            </Link>
+                        </div>
+                    </CardContent>
+                </Card>
+            ))}
+        </div>
+    )
+}
