21a36c0b8195c02c535e5cd1920bd1901e4283df
Johnny Ruiz <johnny@ruizops.com>
2023-07-03 00:01:54 -0600

    updated user loading state

--- app/user/[id]/loading.tsx
+++ app/user/[id]/loading.tsx
@@ -1,4 +1,10 @@
-import { Card, CardFooter, CardHeader } from "@/components/ui/card"
+import {
+    Card,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 
 const Loading = () => {
@@ -6,9 +12,13 @@ const Loading = () => {
         <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
             <Card className="w-1/2">
                 <CardHeader>
-                    <Skeleton className="h-4 w-16" />
-                    <Skeleton className="w-128 h-4" />
-                    <Skeleton className="h-4 w-64" />
+                    <CardTitle className="text-2xl">
+                        <Skeleton className="h-4 w-16" />
+                    </CardTitle>
+                    <CardDescription>
+                        <Skeleton className="w-128 h-4" />
+                        <Skeleton className="h-4 w-64" />
+                    </CardDescription>
                 </CardHeader>
                 <CardFooter className="flex space-x-4 text-sm text-muted-foreground">
                     <Skeleton className="h-8 w-16" />
