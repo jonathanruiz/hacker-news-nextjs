34c13fcbc63c142ea88c1fa518185d3b0c272ab9
Johnny Ruiz <johnny@ruizops.com>
2023-07-02 18:55:34 -0600

    fixed typo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { getStories } from "@/lib/hackerNews"
-import { NewStoryItem, StoryItem } from "@/components/story-item"
+import { StoryItem } from "@/components/story-item"
 
 const IndexPage = async () => {
     const stories = await getStories()
@@ -12,11 +12,7 @@ const IndexPage = async () => {
                     Hacker News Clone with Next.js 13
                 </h1>
                 {stories.map((story: any) => (
-                    <NewStoryItem
-                        key={story.id}
-                        story={story}
-                        stories={stories}
-                    />
+                    <StoryItem key={story.id} story={story} stories={stories} />
                 ))}
             </div>
         </section>
